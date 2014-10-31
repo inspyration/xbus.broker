4b91fa4080b87cdb51c57a09014fb55daa10e338
Florent Aide <florent.aide@gmail.com>
2014-10-31 12:44:12 +0100

    space in docstring

--- xbus/broker/core/back.py
+++ xbus/broker/core/back.py
@@ -68,6 +68,7 @@ class XbusBrokerBack(XbusBrokerBase):
 
         :param token:
          the token you want to invalidate
+
         :return:
          True if successful, False otherwise
         """
