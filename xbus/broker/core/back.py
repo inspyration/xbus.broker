1c7c56929eb4969b75f83eeffe24e209857030f0
Florent Aide <florent.aide@gmail.com>
2014-10-31 16:11:07 +0100

    added stub methods and their documentation

--- xbus/broker/core/back.py
+++ xbus/broker/core/back.py
@@ -31,6 +31,9 @@ class XbusBrokerBack(XbusBrokerBase):
         super(XbusBrokerBack, self).__init__(*args, **kwargs)
         self.node_registry = {}
 
+        self.envelopes = []
+        self.events = {}
+
     @rpc.method
     @asyncio.coroutine
     def login(self, login: str, password: str) -> str:
@@ -115,6 +118,147 @@ class XbusBrokerBack(XbusBrokerBase):
         # later-on when we have work for him to do
         self.node_registry[role_login] = node_client
         return True
+
+    @rpc.method
+    @asyncio.coroutine
+    def start_envelope(self, envelope_id: str) -> str:
+        """Start a new envelop giving its envelop UUID.
+        This is just a way to register the envelop existance with the broker
+        backend. This permits to cancel this envelop further down the road.
+
+        :param envelope_id:
+         the UUID of the envelop you want to start
+        expressed as a string
+
+        :return:
+         the envelop id you just started
+        """
+        self.envelopes.append(envelope_id)
+        return envelope_id
+
+    @rpc.method
+    @asyncio.coroutine
+    def end_envelope(self, envelope_id: str) -> str:
+        """
+        :param envelope_id:
+         the envelope id you want to mark as finished
+
+        :return:
+         a dict containing information about the result like so
+         {'success': True, 'message': "1200 lines inserted, import_id: 23455"}
+        """
+        # TODO: we must call all the active consumers that are concerned by
+        # the envelope we are ending and make sure they are aware they can
+        # now commit their respective changes
+        return {
+            'success': False,
+            'envelope_id': envelope_id,
+            'message': 'broker backend not yet implemented'
+        }
+
+    @rpc.method
+    @asyncio.coroutine
+    def cancel_envelope(self, envelope_id: str) -> str:
+        """This is used to cancel a previously started envelop and make sure
+        the consumers will rollback their changes
+
+        :param envelope_id:
+         the UUID of the envelope you want to cancel
+
+        :return:
+         the UUID of the envelope that has just been cancelled
+        """
+        # TODO: call all the active consumers that are concerned so they can
+        # rollback their current work on the envelope
+        return envelope_id
+
+    @rpc.method
+    @asyncio.coroutine
+    def start_event(
+            self, envelope_id: str, event_id: str, targets=None
+    ) -> tuple:
+        """Begin a new event inside an envelope opened against this broker
+        backend.
+
+        :param envelope_id:
+         the previously opened envelope UUID to which this event is attached.
+         If the envelope is unknown to the broker backend this will return an
+         error code.
+
+        :param event_id:
+         the UUID representing the new event. If the UUID is already known to
+         the broker backend (ie: already in use at the moment. We won't verify
+         in the whole event history) then this method will return an error code
+         instead of processing your data.
+
+        :param targets:
+         the list of consumer ids you want to specifically target with this
+         event. It is optional and in most normal cases (ie: first time the
+         frontend sends an event) it will not be used. The rationale behind
+         this is that you may have situations when a specific node failed
+         and the bus knows that it could not try to send data to the
+         consumers that were situated after the failed node.
+         In this particular situation the bus will mark the consumers as "not
+         properly finished" and stamp the envelope as "not properly finished".
+         When a human operator sees this situation in the management console
+         they will be able to correct the defect and ask for a replay of the
+         failed branches.
+         This will re-emit the event through a subset of the network composed
+         of the branches that lead to "not properly finished" consumers.
+
+        :return:
+         a 2 tuple with the success code and a message:
+
+           - success -> (0, '<event_id>')
+           - failure -> (1, "No such envelope : 87654345678")
+        """
+        if envelope_id in self.envelopes and not event_id in self.events:
+            res = (0, "{}".format(event_id))
+
+        else:
+            msg = ""
+            if envelope_id not in self.envelopes:
+                msg += "No such envelope : {}\n".format(envelope_id)
+
+            if event_id not in self.events:
+                msg += "Event already started: {}\n".format(event_id)
+
+            res = (1, msg)
+
+        return res
+
+    @rpc.method
+    @asyncio.coroutine
+    def end_event(self, event_id: str):
+        """Finish an event normally.
+
+        :param event_id:
+         the event UUID you previously started
+
+        :return:
+         to be defined
+        """
+        pass
+
+    @rpc.method
+    @asyncio.coroutine
+    def send_item(self, event_id: str, data: bytes):
+        """Send an item to the XBUS network.
+
+        :param event_id:
+         event UUID previously opened onto which this item will be sent
+
+        :param data:
+         the raw data of the item. This data will not be opened or
+         interpreted by the bus itself, but forwarded to all the workers and
+         ultimately the consumers of the graph.
+
+        :return:
+         to be defined
+        """
+        # if we have an event_id this means we already have a precomputed graph
+        # for this event... so lets send the item to the corresponding nodes
+        pass
 
     @asyncio.coroutine
     def find_role_by_login(self, login: str) -> tuple:
