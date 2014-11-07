996266852824578c070975c0bf8eaf330daaa092
Florent Aide <florent.aide@gmail.com>
2014-11-07 11:06:47 +0100

    Constructors return None

--- xbus/broker/core/event.py
+++ xbus/broker/core/event.py
@@ -6,7 +6,7 @@ class Event(object):
     """an Event instance represents the event datastructure that is manipulated
     by the backend and dispatched to all workers and consumers that need it"""
 
-    def __init__(self, uuid: str, type_name: str, type_id: str) -> Event:
+    def __init__(self, uuid: str, type_name: str, type_id: str):
         """create a new event instance that will be manipulated by the backend,
         it provides a few helper methods and some interesting attributes like
         the event type name and event type id
